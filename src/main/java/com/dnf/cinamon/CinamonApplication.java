dc941cd493495d8560a2102603b0da2cb817211b
hhegi <larbillilith@gmail.com>
2025-03-01 16:42:42 +0900

    init

---
+++ src/main/java/com/dnf/cinamon/CinamonApplication.java
@@ -0,0 +1,13 @@
+package com.dnf.cinamon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinamonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinamonApplication.class, args);
+	}
+
+}
